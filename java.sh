dea839fe6aa1210ed32bab19c0484968fbf088c1
Brendan O'Connor <brenocon@gmail.com>
2015-01-30 14:30:29 -0500

    outside world-friendly java.sh

--- java.sh
+++ java.sh
@@ -4,7 +4,7 @@ h=$(dirname $0)
 CP=
 CP=$h/bin:$CP
 CP=$CP:$(print -l $h/lib/*.jar | tr '\n' :)
-CP=$CP:$HOME/myutil/bin
+# CP=$CP:$HOME/myutil/bin
 # CP=$CP:$(print -l $HOME/myutil/lib/*.jar | tr '\n' :)
 set -eu
 java -ea -cp "$CP" "$@"
