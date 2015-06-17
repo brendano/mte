4992ecd41c18af6951ad60a9d2443a01f6461c54
Brendan O'Connor <brenocon@gmail.com>
2015-06-16 20:44:33 -0400

    simplify java runner

--- java.sh
+++ java.sh
@@ -1,12 +1,7 @@
 #!/bin/zsh
 # Set up classpath for running eclipse's compilation of the code
 h=$(dirname $0)
-CP=
-CP=$h/bin:$CP
-CP=$CP:$(print -l $h/lib/*.jar | tr '\n' :)
-# CP=$CP:$HOME/myutil/bin
-# CP=$CP:$(print -l $HOME/myutil/lib/*.jar | tr '\n' :)
-#
+CP=$h/bin:$h/lib/*
 set -eu
 set -x
 java -ea -cp "$CP" "$@"
